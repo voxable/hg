2a15fbff9c2f836baa39a5cf32406566e86b0466
Matt Buck <github@matthewbuck.com>
2017-01-24 15:59:59 -0600

    Add initial specs

---
+++ spec/hg/chunk_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+class Bot
+  include Hg::Bot
+end
+
+describe Hg::Chunk do
+  describe '#text' do
+    class Bot
+      module Chunks
+        class ChunkWithText
+          include Hg::Chunk
+
+          text = 'some text'
+        end
+      end
+    end
+
+    it 'generates a proper structured message for a text message' do
+      expect(Bot::Chunks::ChunkWithText.deliverables.first).to eq(
+        {
+          message: {
+            text: message
+          }
+        }
+      )
+    end
+  end
+end
