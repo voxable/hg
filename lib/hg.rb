64c85d64e65ac7bf006e776e282b7563327e63e5
Matt Buck <github@matthewbuck.com>
2017-02-27 18:03:28 -0600

    Drop some misplaced config
    If this had remained where it was, it would have been possible to only
    one run bot at a time. This config belongs on the bot’s class.

--- lib/hg.rb
+++ lib/hg.rb
@@ -15,23 +15,7 @@ require 'hg/router'
 Hashie.logger = Rails.logger
 
 module Hg
-  # The root class for the bot.
-  mattr_accessor :bot_class
-
+  # TODO: Move to Bot itself, default to User.
   # The class representing bot users.
   mattr_accessor :user_class
-
-  module_function
-
-  # @return [Class] The router class for the bot.
-  def router
-    bot_class::Router
-  end
-
-  # @return [Hashie::Mash] The routes map for the bot.
-  def routes
-    # Assume that a router has been defined.
-    # TODO: Test that a router class has been defined on init.
-    router.routes
-  end
 end
--- spec/hg_spec.rb
+++ spec/hg_spec.rb
@@ -8,34 +8,4 @@ describe Hg do
   it 'has a version number' do
     expect(Hg::VERSION).not_to be nil
   end
-
-  context 'configuration' do
-    describe '.bot_class' do
-      it "sets the bot's root class" do
-        Hg.bot_class = Bot
-
-        expect(Hg.bot_class).to eq(Bot)
-      end
-    end
-
-    describe '.router' do
-      it "returns the bot's router class" do
-        expect(Hg.router).to eq(Bot::Router)
-      end
-    end
-
-    describe '.user_class' do
-      it "sets the bot's user class" do
-        Hg.user_class = BotUser
-
-        expect(Hg.user_class).to eq(BotUser)
-      end
-    end
-
-    describe '.routes' do
-      it "returns the bot's routes" do
-        expect(Hg.routes).to_not be_nil
-      end
-    end
-  end
 end
