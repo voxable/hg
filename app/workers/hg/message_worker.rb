1ba4a0bc92ff12c9c0a108c563b632cbc51cb6fb
Matt Buck <github@matthewbuck.com>
2017-03-29 17:25:01 -0500

    Refactor workers to always attempt to drain queues

--- app/workers/hg/message_worker.rb
+++ app/workers/hg/message_worker.rb
@@ -7,62 +7,91 @@ module Hg
 
     # Process an inbound message.
     #
-    # @param user_id [String, Integer] The ID representing the user on this platform
-    # @param redis_namespace [String] The redis namespace under which the message to
-    #   process is nested.
+    # @param user_id [String, Integer] The ID representing the user on this
+    #   platform
+    # @param redis_namespace [String] The redis namespace under which the
+    #   message to process is nested.
     # @param bot_class_name [String] The string version of the bot's class name
     #
     # @return [void]
     def perform(user_id, redis_namespace, bot_class_name)
       # Retrieve the latest message for this user.
-      raw_message = pop_from_queue(
+      raw_message = fetch_raw_message(user_id, redis_namespace)
+
+      # Do nothing if no message available. This could be due to multiple
+      # execution on the part of Sidekiq. This ensures idempotence. We loop
+      # here to ensure that this worker attempts to drain the queue for
+      # the user.
+      while raw_message != {}
+        # Instantiate a message object with the raw message from the queue.
+        message = Facebook::Messenger::Incoming::Message.new(raw_message)
+
+        # Locate the class representing the bot.
+        bot = Kernel.const_get(bot_class_name)
+
+        # Fetch the User representing the message's sender.
+        # TODO: pass in a `user_id_field` to indicate how to find user in order to
+        # make this platform agnostic
+        user = find_bot_user(bot, user_id)
+
+        # If the message is a quick reply...
+        if quick_reply_payload = message.quick_reply
+          # Parse the JSON from the payload.
+          payload = JSON.parse(quick_reply_payload)
+          # ...build a request object from the payload.
+          request = build_payload_request(payload, user)
+        # If the message is text...
+        else
+          # ...send the message to API.ai for NLU.
+          nlu_response = ApiAiClient.new(user.api_ai_session_id).query(message.text)
+
+          # Drop any params that weren't recognized.
+          params = nlu_response[:parameters].reject { |k, v| v.blank? }
+        end
+
+        # Send the request to the bot's router.
+        bot.router.handle(build_request(message, nlu_response, params, user))
+
+        # Attempt to pop another message from the queue for processing.
+        raw_message = fetch_raw_message(user_id, redis_namespace)
+      end
+    end
+
+    private
+
+    # Generate a new request for this message.
+    #
+    # @param message [Facebook::Messenger::Incoming::Message] The incoming message.
+    # @param nlu_response [Hash] The API.ai query response.
+    # @param params [Hash] The parsed entities for the request.
+    # @param user [Object] The user that sent the message.
+    #
+    # @return [Hg::Request] The generated request.
+    def build_request(message, nlu_response, params, user)
+      Hg::Request.new(
+        user:       user,
+        message:    message,
+        intent:     nlu_response[:intent],
+        action:     nlu_response[:action] || nlu_response[:intent],
+        parameters: params,
+        response:   nlu_response[:response]
+      )
+    end
+
+    # Pop the latest raw message from this user's queue.
+    #
+    # @param user_id [String, Integer] The ID representing the user on this
+    #   platform
+    # @param redis_namespace [String] The redis namespace under which the
+    #   message to process is nested.
+    #
+    # @return [Hash] The latest raw message from this user's queue.
+    def fetch_raw_message(user_id, redis_namespace)
+      pop_from_queue(
         Hg::Queues::Messenger::MessageQueue,
-        user_id: user_id,
+        user_id:   user_id,
         namespace: redis_namespace
       )
-
-      # Do nothing if no message available. This could be due to multiple execution
-      # on the part of Sidekiq. This ensures idempotence.
-      return nil if raw_message.empty?
-
-      # Instantiate a message object with the raw message from the queue.
-      message = Facebook::Messenger::Incoming::Message.new(raw_message)
-
-      # Locate the class representing the bot.
-      bot = Kernel.const_get(bot_class_name)
-
-      # Fetch the User representing the message's sender.
-      # TODO: pass in a `user_id_field` to indicate how to find user in order to
-      # make this platform agnostic
-      user = find_bot_user(bot, user_id)
-
-      # If the message is a quick reply...
-      if quick_reply_payload = message.quick_reply
-        # Parse the JSON from the payload.
-        payload = JSON.parse(quick_reply_payload)
-        # ...build a request object from the payload.
-        request = build_payload_request(payload, user)
-      # If the message is text...
-      else
-        # ...send the message to API.ai for NLU.
-        nlu_response = ApiAiClient.new(user.api_ai_session_id).query(message.text)
-
-        # Drop any params that weren't recognized.
-        params = nlu_response[:parameters].reject { |k, v| v.blank? }
-
-        # Build a request object.
-        request = Hg::Request.new(
-          user: user,
-          message: message,
-          intent: nlu_response[:intent],
-          action: nlu_response[:action] || nlu_response[:intent],
-          parameters: params,
-          response: nlu_response[:response]
-        )
-      end
-
-      # Send the request to the bot's router.
-      bot.router.handle(request)
     end
   end
 end
--- app/workers/hg/postback_worker.rb
+++ app/workers/hg/postback_worker.rb
@@ -16,32 +16,44 @@ module Hg
     # @return [void]
     def perform(user_id, redis_namespace, bot_class_name)
       # Retrieve the latest postback for this user
-      raw_postback = pop_from_queue(
+      raw_postback = pop_raw_postback(user_id, redis_namespace)
+
+      # Do nothing if no postback available. This could be due to multiple
+      # execution on the part of Sidekiq. This ensures idempotence. We loop
+      # here to ensure that this worker attempts to drain the queue for
+      # the user.
+      while raw_postback != {}
+        # Extract the payload from the postback.
+        payload = Facebook::Messenger::Incoming::Postback.new(raw_postback).payload
+
+        # Locate the class representing the bot.
+        bot = Kernel.const_get(bot_class_name)
+
+        # Fetch the User representing the message's sender
+        # TODO: pass in a `user_id_field` to indicate how to find user in order to
+        # make this platform agnostic
+        user = find_bot_user(bot, user_id)
+
+        # Build a request object.
+        request = build_payload_request(payload, user)
+
+        # Send the request to the bot's router.
+        bot.router.handle(request)
+
+        # Attempt to pop another postback from the queue for processing.
+        raw_postback = fetch_raw_postback(user_id, redis_namespace)
+      end
+    end
+
+    private
+
+    # @return [Hash] The latest raw postback from this user's queue.
+    def pop_raw_postback(user_id, redis_namespace)
+      pop_from_queue(
         Hg::Queues::Messenger::PostbackQueue,
-        user_id: user_id,
+        user_id:   user_id,
         namespace: redis_namespace
       )
-
-      # Do nothing if no postback available. This could be due to multiple execution on the part of Sidekiq.
-      # This ensures idempotence.
-      return nil if raw_postback.empty?
-
-      # Extract the payload from the postback.
-      payload = Facebook::Messenger::Incoming::Postback.new(raw_postback).payload
-
-      # Locate the class representing the bot.
-      bot = Kernel.const_get(bot_class_name)
-
-      # Fetch the User representing the message's sender
-      # TODO: pass in a `user_id_field` to indicate how to find user in order to
-      # make this platform agnostic
-      user = find_bot_user(bot, user_id)
-
-      # Build a request object.
-      request = build_payload_request(payload, user)
-
-      # Send the request to the bot's router.
-      bot.router.handle(request)
     end
   end
 end
