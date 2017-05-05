5f83b664f6d21497e0b31d86c9e4ba10bc57e32f
Matt Buck <github@matthewbuck.com>
2017-05-05 17:24:44 -0500

    Don’t pass user as a result of to: option
    The to: option should only be used to render static chunks.

--- lib/hg/controllers/chunks_controller.rb
+++ lib/hg/controllers/chunks_controller.rb
@@ -7,11 +7,9 @@ module Hg
       def display_chunk
         # TODO: Shouldn't be constantizing user input. Need a way to sanitize this.
         # Although - payloads shouldn't be something the client is allowed to set, in Messenger.
-        # TODO: Also, use Kernel.const_get https://gist.github.com/Haniyya/0d52fb8ae4c3cb3d46a07fc4180c3303
         chunk_class = Kernel.const_get(params[:chunk])
-        respond_with chunk_class, context: {
-          user: user
-        }
+
+        respond_with chunk_class
       end
     end
   end
