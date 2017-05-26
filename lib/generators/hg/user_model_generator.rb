628e428b83a6c3ed281a25e8f03a168114a4b542
Matt Buck <github@matthewbuck.com>
2017-05-26 18:58:19 -0500

    Improve the user model generator
    OS-16

--- lib/generators/hg/user_model_generator.rb
+++ lib/generators/hg/user_model_generator.rb
@@ -1,13 +1,13 @@
 class Hg::UserModelGenerator < Rails::Generators::Base
-  # Generate a user model and migration.
+  desc 'Generate a user model and migration'
+
+  # Generate a new user model.
   def create_user_model
     generate 'model', 'user facebook_psid:string:index api_ai_session_id:string'
+  end
 
-    # Determine the name of the migration file.
-    create_users_migration_path = Dir.glob(File.join(Rails.root, 'db', 'migrate', "*create_users.rb"))[0]
-
-
-    # Add line enabling hstore
+  # Enable the hstore extension.
+  def enable_hstore
     inject_into_file create_users_migration_path,
                      after: "def change\n" do
       <<-'RUBY'
@@ -15,13 +15,22 @@ class Hg::UserModelGenerator < Rails::Generators::Base
 
       RUBY
     end
+  end
 
-    # Add line adding context field
+  # Add a context field to the migration.
+  def add_context_field
     inject_into_file create_users_migration_path,
                      after: "t.string :api_ai_session_id\n" do
       <<-'RUBY'
       t.hstore :context, default: {}
       RUBY
     end
+  end
+
+  private
+
+  # Determine the name of the migration file.
+  def create_users_migration_path
+    @migration_path ||= Dir.glob(File.join(Rails.root, 'db', 'migrate', "*create_users.rb"))[0]
   end
 end
