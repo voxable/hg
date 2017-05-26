a5df65f004a260e848d118920dfbb91f5bbd9bc5
Aaron Hunter <aaron@voxable.io>
2017-05-26 18:11:38 -0500

    Add base controller generator
    OS-16

---
+++ lib/generators/hg/base_controller_generator.rb
@@ -0,0 +1,14 @@
+class BaseControllerGenerator < Rails::Generators::NamedBase
+  def create_controller_file
+    create_file "app/bot/#{bot_name}_bot/controllers/bot_controller.rb",
+                <<-FILE
+class #{bot_name}Bot
+  module Controllers
+    class BotController < Hg::Controller
+    end
+  end
+end
+
+    FILE
+  end
+end
