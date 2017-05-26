34d2557ac39743801bed13af279618a87de7e6a0
Aaron Hunter <aaron@voxable.io>
2017-05-26 18:12:31 -0500

    Add bot controller generator and template
    OS-16

---
+++ lib/generators/hg/bot_controller.rb
@@ -0,0 +1,11 @@
+module BotController
+  class Generator < Rails::Generators::NamedBase
+    def self.source_root
+      File.expand_path("../templates", __FILE__)
+    end
+
+    def generate
+      template "#{self.class.generator_name}.erb", "app/bot/#{bot_name}/controllers/#{file_name}.rb"
+    end
+  end
+end
--- lib/generators/hg/bot_controller_generator.rb
+++ lib/generators/hg/bot_controller_generator.rb
@@ -1,14 +1,4 @@
-class BotControllerGenerator < Rails::Generators::NamedBase
-  def create_controller_file
-    create_file "app/bot/#{class_name}_bot/controllers/bot_controller_test.rb",
-                <<-FILE
-class #{class_name}Bot
-  module Controllers
-    class BotController < Hg::Controller
-    end
-  end
-end
+require 'generators/hg/bot_controller'
 
-    FILE
-  end
+class BotControllerGenerator < BotController::Generator
 end
