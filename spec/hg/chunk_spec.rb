7692edc39b64a5b8a881b4c866df6aafec2249ef
Bonnie M <blmattson@gmail.com>
2017-06-09 14:30:18 -0500

    add test for chunk attachment

--- spec/hg/chunk_spec.rb
+++ spec/hg/chunk_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-=begin
+
 class Bot
   include Hg::Messenger::Bot
 
@@ -24,11 +24,41 @@ describe Hg::Chunk do
       expect(Bot::Chunks::ChunkWithText.deliverables.first).to eq(
         {
           message: {
-            text: message
+            text: 'some text'
           }
         }
       )
     end
   end
+
+  describe '#attachment' do
+    let(:type){'image'}
+    let(:url){'www.example.com'}
+
+    it 'generates a proper structured message for a text message' do
+
+      class Bot
+        module Chunks
+          class ChunkWithAttachment
+            include Hg::Chunk
+
+            attachment(:type, :url)
+          end
+        end
+      end
+      expect(Bot::Chunks::ChunkWithAttachment.deliverables.first).to eq(
+       {
+         message: {
+           attachment: {
+             type: :type,
+             payload: {
+               url: :url
+             }
+           }
+         }
+       })
+    end
+
+  end
 end
-=end
+
