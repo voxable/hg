280daafe1ce92c5df7a4b2675642c37ddce36896
Aaron Hunter <aaron@voxable.io>
2017-07-06 13:47:40 -0500

    Add it block to spec to separate expects

--- spec/hg/workers/worker_spec_shared_examples.rb
+++ spec/hg/workers/worker_spec_shared_examples.rb
@@ -1,7 +1,11 @@
 RSpec.shared_examples 'a message processing worker' do
-  it "pops the latest unprocessed message from the user's queue" do
-    # TODO: two expectations in one it block is poor form
+  it 'finds the proper queue' do
     expect(queue_class).to receive(:new).with(hash_including(user_id: user_id)).and_return(queue)
+
+    subject.perform(*valid_args)
+  end
+
+  it "pops the latest unprocessed message from the user's queue" do
     expect(queue).to receive(:pop)
 
     subject.perform(*valid_args)
