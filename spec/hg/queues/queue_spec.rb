df3c6aec1c77986092910f7cff81b99608ac1ec4
Aaron Hunter <aaron@voxable.io>
2017-07-14 16:33:33 -0500

    properly stub redis queue methods

--- spec/hg/queues/queue_spec.rb
+++ spec/hg/queues/queue_spec.rb
@@ -6,6 +6,12 @@ describe Hg::Queues::Queue do
   let(:queue) { Hg::Queues::Queue.new(queue_key) }
   let(:some_message) { 'some_message' }
   let(:some_other_message) { 'a newer message' }
+  let(:conn) { double('conn') }
+
+  before(:example) do
+    allow(queue_class).to receive(:initialize).and_return(queue)
+    allow(Hg::Redis).to receive(:execute).and_yield(conn)
+  end
 
   describe '.initialize' do
     it 'assigns redis @key' do
@@ -16,24 +22,13 @@ describe Hg::Queues::Queue do
   end
 
   describe '.pop' do
-    before(:each) do
-      allow(queue_class).to receive(:initialize).and_return(queue)
-    end
-
     context 'when there are messages on the queue' do
       it 'pops the oldest message off of the given queue' do
-        queue.push(some_message)
-        queue.push(some_other_message)
+        allow(conn).to receive(:lpop).and_return(JSON.generate(some_message))
 
         result = queue.pop
 
         expect(result).to eq some_message
-      end
-
-      it 'then pops the next in queue' do
-        result = queue.pop
-
-        expect(result).to eq some_other_message
       end
     end
 
@@ -42,6 +37,8 @@ describe Hg::Queues::Queue do
       let(:empty_queue) { Hg::Queues::Queue.new(empty_queue_key) }
 
       it 'returns an empty hash' do
+        allow(conn).to receive(:lpop).and_return(nil)
+
         result = empty_queue.pop
 
         expect(result).to be_empty
@@ -51,7 +48,7 @@ describe Hg::Queues::Queue do
 
   describe '.push' do
     it 'puts the message on the given queue' do
-      allow(Hg::Redis).to receive(:execute).and_return('1')
+      allow(conn).to receive(:rpush).and_return(JSON.generate('1'))
 
       result = queue.push(some_message)
 
