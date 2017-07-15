40c008e418ea772499f81e26ff5b29cb52c3fbc9
Aaron Hunter <aaron@voxable.io>
2017-07-15 14:13:24 -0500

    WIP Fill out some specs

--- spec/hg/api_ai_client_spec.rb
+++ spec/hg/api_ai_client_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe Hg::ApiAiClient do
   let(:message) { 'somemessage' }
   let(:token) { 'sometoken' }
   let(:session_id) { '123456' }
-  let(:api_ai_response) { { intent: nil, action: nil, parameters: { foo: 1 } }}
   let(:api_ai_ruby_client) {
     instance_double('ApiAiRuby::Client')
   }
@@ -13,23 +12,15 @@ RSpec.describe Hg::ApiAiClient do
     allow(ApiAiRuby::Client).to receive(:new).and_return(api_ai_ruby_client)
   end
 
-  describe '.initialize' do
-    it 'creates api.ai client' do
+  describe '#initialize' do
+    it 'creates api.ai client and sets session_id' do
       result = Hg::ApiAiClient.new(session_id)
 
-      expect(result.instance_variable_get(:@client)).to_not be_nil
-    end
-
-    context 'setting @client' do
-      it 'has a client access token'
-
-      it 'has the api version'
-
-      it 'has the session_id'
+      expect(result.instance_variable_get(:@client)['session_id']).to eq session_id
     end
   end
 
-  describe '.query' do
+  describe '#query' do
     context 'when the query request fails' do
       it 'logs error to Sidekick::Logger'
 
@@ -46,7 +37,28 @@ RSpec.describe Hg::ApiAiClient do
 
     context 'with valid query/response' do
       context 'with no action name defined for intent' do
-        it 'uses intent name'
+        let(:no_action_response) {
+          {
+            result: {
+              metadata: {
+                intentName: 'someintent'
+              },
+              action: '',
+              fulfillment: {
+                speech: 'someresponse'
+              },
+              parameters: 'someparams'
+            }
+          }
+        }
+
+        it 'uses intent name' do
+          allow(ApiAiRuby::Client).to receive(:text_request).and_return(no_action_response)
+
+          result = subject.query(message)
+
+          expect(result.action).to eq 'someintent'
+        end
       end
 
       context 'when message is not recognized' do
@@ -54,15 +66,70 @@ RSpec.describe Hg::ApiAiClient do
       end
 
       context 'when message is recognized' do
-        it 'returns intent name'
+        let(:api_ai_response) {
+          {
+            result: {
+              metadata: {
+                intentName: 'someintent'
+              },
+              action: 'someaction',
+              fulfillment: {
+                speech: 'someresponse'
+              },
+              parameters: 'someparams'
+            },
+            status: {
+              code: 200
+            }
+          }
+        }
+        let(:response_blank_params) {
+          {
+            result: {
+              parameters: {
+                thiskey: 'hassomething',
+                thatkey: 'alsoiswith',
+                butthisemptykey: ''
+              }
+            }
+          }
+        }
 
-        it 'returns action name'
+        before(:example) do
+          allow(ApiAiRuby::Client).to receive(:text_request).and_return(api_ai_response)
+        end
 
-        it 'returns provided params'
+        it 'returns the response' do
+          result = subject.query(message)
 
-        it 'removes blank params'
+          expect(result).to eq api_ai_response
+        end
 
-        it 'returns response'
+        it 'returns intent name' do
+          result = subject.query(message)
+
+          expect(result.intent).to eq 'someintent'
+        end
+
+        it 'returns action name' do
+          result = subject.query(message)
+
+          expect(result.action).to eq 'someaction'
+        end
+
+        it 'returns provided params' do
+          result = subject.query(message)
+
+          expect(result.parameters).to eq 'someparams'
+        end
+
+        it 'removes blank params' do
+          allow(ApiAiRuby::Client).to receive(:text_request).and_return(response_blank_params)
+
+          result = subject.query(message)
+
+          expect(result.parameters).to be hash_excluding(butthisemptykey: '')
+        end
       end
     end
   end
