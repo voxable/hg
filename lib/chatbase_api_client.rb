31e8f08b807f4dc486f463f14e3c1071e7cd1559
Aaron Hunter <aaron@voxable.io>
2018-01-13 09:30:23 -0600

    Serialize data for Chatbase delivery
    SXSW-11

--- lib/chatbase_api_client.rb
+++ lib/chatbase_api_client.rb
@@ -6,21 +6,99 @@ class ChatbaseAPIClient
   BASE_PATH = 'https://chatbase.com/api/facebook'
 
   # Sends message sent by user to Chatbase
+  #
+  # @param [Hash] message
+  #   Message data received
+  #
+  # @return [void]
   def send_user_message(message)
+    # Format for Chatbase Facebook API
+    message_received = serialize_user_message(message)
+    # Post to Chatbase
     catch_errors{
-      self.class.post("#{BASE_PATH}/message_received?api_key=#{ENV['CHATBASE_API_KEY']}", json_body(message))
+      self.class.post("#{BASE_PATH}/message_received?api_key=#{ENV['CHATBASE_API_KEY']}", json_body(message_received))
     }
   end
 
   # Sends message sent by bot to Chatbase
-  def send_bot_message(message)
+  #
+  # @param [String] text
+  #   Message text
+  # @param [Hash] response
+  #   Hash of response data from Facebook
+  #
+  # @return [void]
+  def send_bot_message(text, response)
+    # Format for Chatbase Facebook API
+    message_sent = serialize_bot_message(text, response)
+    # Post to Chatbase
     catch_errors{
-      self.class.post("#{BASE_PATH}/send_message?api_key=#{ENV['CHATBASE_API_KEY']}", json_body(message))
+      self.class.post("#{BASE_PATH}/send_message?api_key=#{ENV['CHATBASE_API_KEY']}", json_body(message_sent))
     }
   end
 
   private
 
+  # Formats received message data for Chatbase Facebook API
+  #
+  # @param [Hash] message
+  #   Hash of message data
+  #
+  # @return [Hash]
+  #   Message data formatted for Chatbase
+  def serialize_user_message(message)
+    if message['postback']
+      intent = message['postback']['payload']['action']
+      text = message['postback']['title']
+    else
+      intent = message.intent || message.action
+      text = message['text']
+    end
+    # Indicates if bot did not respond correctly
+    not_handled = intent.nil?
+
+    message_received = {
+      sender: message['sender']['id'],
+      recipient: message['recipient']['id'],
+      timestamp: message['timestamp'],
+      message: {
+        mid: message['message_id'],
+        text: text
+      },
+      chatbase_fields: {
+        intent: intent,
+        version: ENV['CHATBASE_BOT_VERSION'],
+        not_handled: not_handled
+      }
+    }
+  end
+
+  # Formats sent message data for Chatbase Facebook API
+  #
+  # @param [String] text
+  #   Message text
+  # @param [Hash] response
+  #   Response from Facebook
+  #
+  # @return [Hash]
+  #   Message data formatted for Chatbase
+  def serialize_bot_message(text, response)
+    message_sent = {
+      request_body: {
+        recipient: response['recipient_id'],
+        message: {
+          text: text
+        }
+      },
+      response_body: {
+        recipient_id: response['recipient_id'],
+        message_id: response['message_id']
+      },
+      chatbase_fields: {
+        version: ENV['CHATBASE_BOT_VERSION']
+      }
+    }
+  end
   def catch_errors
     begin
       @response = yield
