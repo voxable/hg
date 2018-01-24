b2ac9198a87a368a54f7ff8b390f2eb61810c94e
Aaron Hunter <aaron@voxable.io>
2018-01-24 14:43:21 -0600

    Include option for external and internal paths

--- lib/hg/chunk.rb
+++ lib/hg/chunk.rb
@@ -302,9 +302,13 @@ module Hg
           title: title
         }
 
-        # If image_url is specified, include the asset path
+        # If image_url is specified include the url or asset path
         if options[:image_url]
-          quick_reply_content[:image_url] = ApplicationController.helpers.image_url(options[:image_url])
+          if options.has_key?(:host)
+            quick_reply_content[:image_url] = ApplicationController.helpers.image_url(options[:image_url], host: options[:host])
+          else
+            quick_reply_content[:image_url] = options[:image_url]
+          end
         end
 
         # If a `to` option is present, assume this is a postback link to another chunk.
